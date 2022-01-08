bdbfdd24918e7d1a6c7b0f09ac9ba2c3563d25e5
Taro Ohnishi <taro@taro.local>
2022-01-08 15:16:25 +0900

    不要なlint設定を削除

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
+import { Link } from "@mui/material";
 import React from "react";
 import "./App.css";
 
 function App() {
   return (
     <>
-      <span>Taroro's Home</span>
+      <h1>Taroro's Home</h1>
+      <h2>Experience</h2>
+      <h2>Links</h2>
+      <ul>
+        <li>GitHub</li>
+        <Link></Link>
+      </ul>
     </>
   );
 }
